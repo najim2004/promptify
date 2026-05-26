e8d76333f2c6b4fa23d5d04f1bebc0aa5fb7a1ab
najim2004 <najim10112004@gmail.com>
2026-05-26 10:37:30 +0600

    feat: implement animated splash screen and add required dependencies for fonts, gradients, and SVG icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,22 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { useState } from 'react';
 import { useColorScheme } from 'react-native';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import PromptNestSplash from '@/components/splash-screen';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [isSplashVisible, setIsSplashVisible] = useState(true);
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
+      {isSplashVisible ? (
+        <PromptNestSplash onFinish={() => setIsSplashVisible(false)} />
+      ) : (
+        <AppTabs />
+      )}
     </ThemeProvider>
   );
 }
+
